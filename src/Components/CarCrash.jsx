2005e86296f9d57f471be2c10814a367b81e5ac7
Vivek Ranjan <infovivek.ranjan@gmail.com>
2021-09-18 15:21:46 +0530

    Loading data in a simple table done

--- src/Components/CarCrash.jsx
+++ src/Components/CarCrash.jsx
@@ -1,11 +1,71 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Css/CarCrash.module.css";
 
 function CarCrash() {
+  const [data, setData] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsEroor] = useState(false);
+
+  const getCrashData = () => {
+    setIsLoading(true);
+
+    return axios
+      .get(`https://data.cityofnewyork.us/resource/h9gi-nx95.json`)
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsEroor(true);
+        setIsLoading(false);
+      })
+      .finally(() => {
+        setIsEroor(false);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getCrashData();
+  }, []);
+
   return (
     <div className={styles.carCrashCont}>
       <header className={styles.headerCont}>
         <h1>Car Crash Data</h1>
+
+        <section>
+          {isLoading ? (
+            <h3>Loading...</h3>
+          ) : isError ? (
+            <h3>Something went wrong...</h3>
+          ) : (
+            <section>
+              <div className={styles.tableHeader}>
+                <div>Car type - 1</div>
+                <div>Car type - 2</div>
+                <div>Crash Date</div>
+                <div>Crash Time</div>
+              </div>
+              <div>
+                {data.map((item) => {
+                  return (
+                    <div key={item.collision_id} className={styles.tableRows}>
+                      <div>{item.vehicle_type_code1}</div>
+                      <div>{item.vehicle_type_code2}</div>
+                      <div>{item.crash_date}</div>
+                      <div>{item.crash_time}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </section>
+          )}
+        </section>
       </header>
     </div>
   );
