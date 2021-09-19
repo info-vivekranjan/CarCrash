ab932dc8b43b5e3f8624090e70757d28771c9132
Vivek Ranjan <infovivek.ranjan@gmail.com>
2021-09-19 10:00:58 +0530

    toggle support to switch between list/card view Done

--- src/Components/CarCrash.jsx
+++ src/Components/CarCrash.jsx
@@ -14,6 +14,8 @@ function CarCrash() {
 
   const { theme } = useContext(CarCrashContext);
 
+  const [showCard, setShowCard] = useState(true);
+
   const getCrashData = (page = 0) => {
     setIsLoading(true);
 
@@ -45,61 +47,126 @@ function CarCrash() {
       <header className={styles.headerCont}>
         <h1>Car Crash Data</h1>
       </header>
-      <section>
-        {isLoading ? (
-          <h3>Loading...</h3>
-        ) : isError ? (
-          <h3>Something went wrong...</h3>
-        ) : (
-          <section>
-            <div
-              className={styles.tableHeader}
-              style={{ border: theme.tableBorder }}
-            >
-              <div style={{ border: theme.tableBorder }}>Car type - 1</div>
-              <div style={{ border: theme.tableBorder }}>Car type - 2</div>
-              <div style={{ border: theme.tableBorder }}>Crash Date</div>
-              <div style={{ border: theme.tableBorder }}>Crash Time</div>
-            </div>
-            <div>
-              {data.map((item) => {
-                return (
-                  <Link
-                    key={item.collision_id}
-                    className={styles.tableRows}
-                    to={item.collision_id}
-                    style={{
-                      border: theme.tableBorder,
-                      borderBottom: theme.tableBorderBottom,
-                      borderTop: theme.tableBorderTop,
-                    }}
-                  >
-                    <div style={{ border: theme.tableBorder }}>
-                      {item.vehicle_type_code1}
-                    </div>
-                    <div style={{ border: theme.tableBorder }}>
-                      {item.vehicle_type_code2}
-                    </div>
-                    <div style={{ border: theme.tableBorder }}>
-                      {item.crash_date}
-                    </div>
-                    <div style={{ border: theme.tableBorder }}>
-                      {item.crash_time}
-                    </div>
-                  </Link>
-                );
-              })}
-            </div>
-            <div className={styles.paginationCont}>
-              <button disabled={page === 0} onClick={() => setPage(page - 15)}>
-                Prev
-              </button>
 
-              <button onClick={() => setPage(page + 15)}>Next</button>
-            </div>
-          </section>
+      <section className={styles.showCardCont}>
+        {showCard ? (
+          <div className={styles.showCardGrid}>
+            <i
+              onClick={() => setShowCard(!showCard)}
+              className="ri-layout-grid-fill"
+            ></i>
+          </div>
+        ) : (
+          <div className={styles.showCardRow}>
+            <i
+              onClick={() => setShowCard(!showCard)}
+              className="ri-layout-row-fill"
+            ></i>
+          </div>
         )}
       </section>
+
+      <>
+        {showCard ? (
+          <section>
+            {isLoading ? (
+              <h3>Loading...</h3>
+            ) : isError ? (
+              <h3>Something went wrong...</h3>
+            ) : (
+              <section>
+                <div
+                  className={styles.tableHeader}
+                  style={{ border: theme.tableBorder }}
+                >
+                  <div style={{ border: theme.tableBorder }}>Car type - 1</div>
+                  <div style={{ border: theme.tableBorder }}>Car type - 2</div>
+                  <div style={{ border: theme.tableBorder }}>Crash Date</div>
+                  <div style={{ border: theme.tableBorder }}>Crash Time</div>
+                </div>
+                <div>
+                  {data.map((item) => {
+                    return (
+                      <Link
+                        key={item.collision_id}
+                        className={styles.tableRows}
+                        to={item.collision_id}
+                        style={{
+                          border: theme.tableBorder,
+                          borderBottom: theme.tableBorderBottom,
+                          borderTop: theme.tableBorderTop,
+                        }}
+                      >
+                        <div style={{ border: theme.tableBorder }}>
+                          {item.vehicle_type_code1}
+                        </div>
+                        <div style={{ border: theme.tableBorder }}>
+                          {item.vehicle_type_code2}
+                        </div>
+                        <div style={{ border: theme.tableBorder }}>
+                          {item.crash_date}
+                        </div>
+                        <div style={{ border: theme.tableBorder }}>
+                          {item.crash_time}
+                        </div>
+                      </Link>
+                    );
+                  })}
+                </div>
+                <div className={styles.paginationCont}>
+                  <button
+                    disabled={page === 0}
+                    onClick={() => setPage(page - 15)}
+                  >
+                    Prev
+                  </button>
+
+                  <button onClick={() => setPage(page + 15)}>Next</button>
+                </div>
+              </section>
+            )}
+          </section>
+        ) : (
+          <section>
+            {isLoading ? (
+              <h3>Loading...</h3>
+            ) : isError ? (
+              <h3>Something went wrong...</h3>
+            ) : (
+              <section>
+                <div className={styles.cardCont}>
+                  {data.map((item) => {
+                    return (
+                      <div className={styles.cardLinkCont}>
+                        <Link
+                          key={item.collision_id}
+                          to={item.collision_id}
+                          className={styles.cardLink}
+                        >
+                          <div>Car type -1 : {item.vehicle_type_code1}</div>
+                          <div>Car type -2 : {item.vehicle_type_code2}</div>
+                          <div>Crash date : {item.crash_date}</div>
+                          <div>Crash time: {item.crash_time}</div>
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className={styles.paginationCont}>
+                  <button
+                    disabled={page === 0}
+                    onClick={() => setPage(page - 15)}
+                  >
+                    Prev
+                  </button>
+
+                  <button onClick={() => setPage(page + 15)}>Next</button>
+                </div>
+              </section>
+            )}
+          </section>
+        )}
+      </>
     </div>
   );
 }
